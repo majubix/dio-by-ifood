252668b92d5a8f0d5d06eefbaea8246e8a3071b2
majubix <maju.rocha@hotmail.com>
2023-07-21 01:32:54 -0300

    modified: operadores de atribuicao

---
+++ operadoresatribuicao.py
@@ -0,0 +1,28 @@
+#atribuição simples
+
+saldo = 500
+print(saldo)
+
+#Atribuição com adição
+
+saldo = 500
+saldo += 200
+
+
+print(saldo)
+
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //- 2
+print(saldo)
+
+saldo *= 10
+print(saldo)
+
